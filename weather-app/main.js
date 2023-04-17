e8dd261e06e98291b9faaf40b171599a8d08c0c0
Cindy Fung <cindy.fungc@gmail.com>
2023-04-17 16:36:41 -0700

    Add transforming the data

--- weather-app/main.js
+++ weather-app/main.js
@@ -2,7 +2,7 @@ import "./style.css"
 import { getWeather } from "./weather"
 
 getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    res => {
-        console.log(res.data)
+    data => {
+        console.log(data)
     }
 )
