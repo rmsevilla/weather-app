4318ea6e01c8551bd5aa5e60921a642bf78165d0
rmsevilla <70169981+rmsevilla@users.noreply.github.com>
2023-04-19 18:18:45 -0700

    Reset

--- weather-app/main.js
+++ weather-app/main.js
@@ -9,10 +9,10 @@ getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
         alert("Error getting weather.")
     })
 
-function renderWeather({current, daily, hourly}) {
+function renderWeather({current, hourly, daily}) {
     renderCurrentWeather(current)
+    renderHourlyWeather(hourly)
     renderDailyWeather(daily)
-    // renderHourlyWeather(hourly)
     document.body.classList.remove("blurred")
 }
 
@@ -34,6 +34,24 @@ function renderCurrentWeather(current) {
     // setValue("current-fl-low", current.lowFeelsLike)
     // setValue("current-wind", current.windSpeed)
     // setValue("current-precip", current.precip)
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: "numeric"})
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourRowTemplate = document.getElementById("hour-row-template")
+function renderHourlyWeather(hourly) {
+    hourlySection.innerHTML = ""
+    hourly.forEach(hour => {
+        const element = hourRowTemplate.content.cloneNode(true)
+        setValue("temp", hour.temp, { parent: element})
+        setValue("fl-temp", hour.feelsLike, { parent: element})
+        setValue("wind", hour.windSpeed, { parent: element})
+        setValue("precip", hour.precip, { parent: element})
+        setValue("day", DAY_FORTMATTER.format(hour.timestamp), { parent: element})
+        setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element})
+        element.querySelector("[data-icon").src = getIconUrl(hour.iconCode)
+        hourlySection.append(element)
+    })
 }
 
 const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, {weekday: "long"})
